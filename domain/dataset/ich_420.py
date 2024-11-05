41ac05e74c3f5ef5669ca57c92ce8d6947ca551e
S.W.-Chen <wenwen357951@gmail.com>
2024-11-05 21:15:37 +0000

    feat: 新增 3d General 特徵解析器

--- domain/dataset/ich_420.py
+++ domain/dataset/ich_420.py
@@ -20,3 +20,23 @@ def general_feature_description():
         .append(ICHFeature.BASELINE_RESIZE_LABEL) \
         .append(ICHFeature.BASELINE_RESIZE_LABEL_SLICES_NUMBER) \
         .build()
+
+def general_3D_feature_description():
+    return FeatureDescriptionBuilder() \
+        .append(ICHFeature.BASELINE_IMAGE) \
+        .append(ICHFeature.BASELINE_IMAGE_HEIGHT) \
+        .append(ICHFeature.BASELINE_IMAGE_WIDTH) \
+        .append(ICHFeature.BASELINE_IMAGE_SLICES_NUMBER) \
+        .append(ICHFeature.BASELINE_IMAGE_FILENAME) \
+        .append(ICHFeature.BASELINE_SLOPE) \
+        .append(ICHFeature.BASELINE_INTER) \
+        .append(ICHFeature.HEMATOMA_EXPANSION) \
+        .append(ICHFeature.MODEL_1_BASELINE_IMAGE) \
+        .append(ICHFeature.MODEL_2_1_BASELINE_IMAGE) \
+        .append(ICHFeature.MODEL_2_2_BASELINE_IMAGE) \
+        .append(ICHFeature.MODEL_4_BASELINE_IMAGE) \
+        .append(ICHFeature.MODEL_X_IMAGE_HEIGHT) \
+        .append(ICHFeature.MODEL_X_IMAGE_WIDTH) \
+        .append(ICHFeature.MODEL_X_IMAGE_DEPTH) \
+        .build()
+
